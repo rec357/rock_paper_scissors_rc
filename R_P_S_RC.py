6e53442c267fbcf895b4b725d0755d0379970d02
Rita Cordero <rec357@stern.nyu.edu>
2022-07-12 21:27:19 -0400

    Update R_P_S_RC.py

--- R_P_S_RC.py
+++ R_P_S_RC.py
@@ -1,18 +1,20 @@
-from tkinter import N
 
 import random
 #we need to add random
 
-
+#introduction to game
 
 options = ["rock", "paper", "scissors"]
 
 print("Rock, Paper, Scissors, Shoot!")
 
+#introduction to human player
 
 name = input("Please enter your name: ")
 
 print("Player One is: {}".format (name))
+
+# game begins
 
 print ("Let's Play!")
 
@@ -21,6 +23,8 @@ player_one_pick = input ("Pick one: Rock, Paper, or Scissors\n")
 computer_player_pick = random.choice (options)
 
 print("The computer choice is: {}".format (computer_player_pick))
+
+# possible outcomes
 
 if player_one_pick == "rock" and computer_player_pick == "rock" : 
     outcome = "tie" 
@@ -53,3 +57,5 @@ else :
     outcome = "invalid!"
 
 print("Outcome is: {}".format (outcome))
+
+print("Thank you for playing!")
